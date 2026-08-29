34c65f4c88dbdcab7f6461b889b4754fdf7bfbd9
signalssoundsandwaves <signalssoundsandwaves@gmail.com>
2026-08-29 08:35:54 +0100

    shell added

--- src/collections.js
+++ src/collections.js
@@ -2,5 +2,5 @@
 // then add its public-relative path to the matching works array below.
 export const collections = [
   { id: "sylvanians", title: "Sylvanian Families", description: "A collection of sylvanians", thumbnail: "/thumbnails/sylvanians.png", works: [{title: "Penguin Chef", src: "/collections/sylvanians/penguinBlackBg_edit.html"}, {title: "Mole", src: "/collections/sylvanians/mole.html"}, {title: "Cat", src: "/collections/sylvanians/cat_edit.html"}, {title: "Bear", src: "/collections/sylvanians/bear_edit.html"}] },
-  { id: "nature", title: "Nature", description: "Shells, fossils, pinecones, and other natural curiosities.", thumbnail: "/thumbnails/nature.png", works: [{title: "Pinecone Macro", src: "/collections/nature/pinecone_macro_edit.html"}, {title: "Fossil", src: "/collections/nature/fossil_edit.html"}] },
+  { id: "nature", title: "Nature", description: "Shells, fossils, pinecones, and other natural curiosities.", thumbnail: "/thumbnails/nature.png", works: [{title: "Pinecone Macro", src: "/collections/nature/pinecone_macro_edit.html"}, {title: "Fossil", src: "/collections/nature/fossil_edit.html"}, {title: "Shell Macro", src: "/collections/nature/shell_macro_edit.html"}] },
 ];
